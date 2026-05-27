9c04d079e7952f234b7a8a94458542269047f995
reshma <21028cm097@gmail.com>
2026-05-27 16:56:11 +0530

    frontend connected to backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://campushub-1-jjze.onrender.com',
         changeOrigin: true,
       }
     }
